3f900ffeb07d72311368ee3e638a3d64c02c36b0
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-05-11 07:16:00 -0500

    Styling for grid on index

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -3,7 +3,7 @@ class WineriesController < ApplicationController
 
   def search 
     if params[:search].present?
-      @wineries = Winery.search(params[:search]).paginate(:page => params[:page], :per_page => 30).order('name ASC')
+      @wineries = Winery.search(params[:search]).paginate(:page => params[:page], :per_page => 32).order('name ASC')
     else
       @wineries = Winery.all
     end
@@ -12,7 +12,7 @@ class WineriesController < ApplicationController
   # GET /wineries
   # GET /wineries.json
   def index
-    @wineries = Winery.all.paginate(:page => params[:page], :per_page => 30).order('name ASC')
+    @wineries = Winery.all.paginate(:page => params[:page], :per_page => 32).order('name ASC')
 
   end
 
