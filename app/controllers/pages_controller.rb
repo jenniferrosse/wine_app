0f2f65b61cd6aec1d32f438ff9c90d1af8c85951
Jennifer Rosse <jenniferrosse26@gmail.com>
2017-01-15 13:48:35 -0600

    Added lists of suggested producers by region to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @producers = Producer.all
+    @producers_california = @producers.where(region: "California").limit(4)
+    @producers_oregon = @producers.where(region: "Oregon").limit(4)
+    @producers_france = @producers.where(country: "France").limit(4)
+
   end
 
   def contact
