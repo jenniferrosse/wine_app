c04671d61db35006f1d83854f957d28086d6a708
Jennifer Rosse <jenniferrosse26@gmail.com>
2017-01-15 14:39:44 -0600

    Resized/styled images for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     @producers = Producer.all.order('name ASC')
-    @producers_california = @producers.where(region: "California").limit(4)
-    @producers_oregon = @producers.where(region: "Oregon").limit(4)
-    @producers_france = @producers.where(country: "France").limit(4)
+    @producers_california = @producers.where(region: "California").limit(6)
+    @producers_oregon = @producers.where(region: "Oregon").limit(6)
+    @producers_france = @producers.where(country: "France").limit(6)
 
   end
 
