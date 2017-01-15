38140789f2b1c06bcad54a2a17f72c632ae5348c
Jennifer Rosse <jenniferrosse26@gmail.com>
2017-01-15 14:23:55 -0600

    Order name asc on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @producers = Producer.all
+    @producers = Producer.all.order('name ASC')
     @producers_california = @producers.where(region: "California").limit(4)
     @producers_oregon = @producers.where(region: "Oregon").limit(4)
     @producers_france = @producers.where(country: "France").limit(4)
